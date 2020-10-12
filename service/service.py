8650ce228ed535c84b1128902b1d0eff124889cb
nancywen25 <nwen2011@gmail.com>
2020-10-11 21:27:55 -0400

    add skeleton code for the service

--- service/service.py
+++ service/service.py
@@ -1,7 +1,16 @@
 """
-My Service
+Customer Service
 
-Describe what your service does here
+PATHS:
+------
+GET /customers - returns a list of all the Customers
+GET /customers/{id} - returns the Customer with a given id number
+POST /customers - creates a new Customer record in teh database
+PUT /customers/{id} - updates a Customer record in the database
+DELETE /customers/{id} - deletes a Customer record in the databse
+
+PUT /customers/{id}/suspend - suspend the Customer with the given id number
+
 """
 
 import os
@@ -13,10 +22,84 @@ from flask_api import status  # HTTP Status Codes
 # For this example we'll use SQLAlchemy, a popular ORM that supports a
 # variety of backends including SQLite, MySQL, and PostgreSQL
 from flask_sqlalchemy import SQLAlchemy
-from service.models import YourResourceModel, DataValidationError
+from service.models import Customer, DataValidationError
 
 # Import Flask application
 from . import app
+
+######################################################################
+# Error Handlers
+######################################################################
+@app.errorhandler(DataValidationError)
+def request_validation_error(error):
+    """ Handles Value Errors from bad data """
+    return bad_request(error)
+
+
+@app.errorhandler(status.HTTP_400_BAD_REQUEST)
+def bad_request(error):
+    """ Handles bad reuests with 400_BAD_REQUEST """
+    app.logger.warning(str(error))
+    return (
+        jsonify(
+            status=status.HTTP_400_BAD_REQUEST, error="Bad Request", message=str(error)
+        ),
+        status.HTTP_400_BAD_REQUEST,
+    )
+
+
+@app.errorhandler(status.HTTP_404_NOT_FOUND)
+def not_found(error):
+    """ Handles resources not found with 404_NOT_FOUND """
+    app.logger.warning(str(error))
+    return (
+        jsonify(
+            status=status.HTTP_404_NOT_FOUND, error="Not Found", message=str(error)
+        ),
+        status.HTTP_404_NOT_FOUND,
+    )
+
+
+@app.errorhandler(status.HTTP_405_METHOD_NOT_ALLOWED)
+def method_not_supported(error):
+    """ Handles unsuppoted HTTP methods with 405_METHOD_NOT_SUPPORTED """
+    app.logger.warning(str(error))
+    return (
+        jsonify(
+            status=status.HTTP_405_METHOD_NOT_ALLOWED,
+            error="Method not Allowed",
+            message=str(error),
+        ),
+        status.HTTP_405_METHOD_NOT_ALLOWED,
+    )
+
+
+@app.errorhandler(status.HTTP_415_UNSUPPORTED_MEDIA_TYPE)
+def mediatype_not_supported(error):
+    """ Handles unsuppoted media requests with 415_UNSUPPORTED_MEDIA_TYPE """
+    app.logger.warning(str(error))
+    return (
+        jsonify(
+            status=status.HTTP_415_UNSUPPORTED_MEDIA_TYPE,
+            error="Unsupported media type",
+            message=str(error),
+        ),
+        status.HTTP_415_UNSUPPORTED_MEDIA_TYPE,
+    )
+
+
+@app.errorhandler(status.HTTP_500_INTERNAL_SERVER_ERROR)
+def internal_server_error(error):
+    """ Handles unexpected server error with 500_SERVER_ERROR """
+    app.logger.error(str(error))
+    return (
+        jsonify(
+            status=status.HTTP_500_INTERNAL_SERVER_ERROR,
+            error="Internal Server Error",
+            message=str(error),
+        ),
+        status.HTTP_500_INTERNAL_SERVER_ERROR,
+    )
 
 ######################################################################
 # GET INDEX
@@ -24,7 +107,82 @@ from . import app
 @app.route("/")
 def index():
     """ Root URL response """
-    return "Reminder: return some useful information in json format about the service here", status.HTTP_200_OK
+    app.logger.info("Request for Root URL")
+    return (
+    	jsonify(
+    		name = "Customer REST API Service",
+    		version="1.0",
+    		paths=url_for("list_customers", _external=True), # url_for() generates the url for a function
+    	),
+    	status.HTTP_200_OK,
+    )
+
+######################################################################
+# LIST ALL CUSTOMERS
+######################################################################
+@app.route("/customers", methods=["GET"])
+def list_customers():
+    """ Returns all of the Customers """
+    # TODO
+
+    # Note: query filtering would also be implemented in this function
+    pass 
+
+######################################################################
+# RETRIEVE A CUSTOMER
+######################################################################
+@app.route("/customers/<int:customer_id>", methods=["GET"])
+def get_customers(customer_id):
+    """
+    Retrieve a single Customer
+
+    This endpoint will return a Customer based on it's id
+    """
+    # TODO
+    pass
+
+######################################################################
+# ADD A NEW CUSTOMER
+######################################################################
+@app.route("/customers", methods=["POST"])
+def create_customers():
+    """
+    Creates a Customer
+    This endpoint will create a Customer based the data in the body that is posted
+    """
+    # TODO
+    pass
+
+
+######################################################################
+# UPDATE AN EXISTING CUSTOMER
+######################################################################
+@app.route("/customers/<int:customer_id>", methods=["PUT"])
+def update_customers(customer_id):
+    """
+    Update a Customer
+
+    This endpoint will update a Customer based the body that is posted
+    """
+    # TODO
+
+    # Note: the action of suspending a customer is also implemented in this function
+    pass
+
+
+######################################################################
+# DELETE A CUSTOMER
+######################################################################
+@app.route("/customers/<int:customer_id>", methods=["DELETE"])
+def delete_customers(customer_id):
+    """
+    Delete a Customer
+
+    This endpoint will delete a Customer based the id specified in the path
+    """
+    # TODO
+    pass
+
 
 
 ######################################################################
@@ -34,4 +192,11 @@ def index():
 def init_db():
     """ Initialies the SQLAlchemy app """
     global app
-    YourResourceModel.init_db(app)
+    Customer.init_db(app)
+
+def check_content_type(content_type):
+    """ Checks that the media type is correct """
+    if request.headers["Content-Type"] == content_type:
+        return
+    app.logger.error("Invalid Content-Type: %s", request.headers["Content-Type"])
+    abort(415, "Content-Type must be {}".format(content_type))
