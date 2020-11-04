fc0096edb36eaf557cf577a4c083dea569f6f99b
Henry-Ou <henryou0506@gmail.com>
2020-11-04 18:19:51 -0500

    remove postgre uri in config file.

--- config.py
+++ config.py
@@ -5,21 +5,20 @@ import os
 import json
 
 # Get configuration from environment
-print("-----Initial DATABASE_URL-----")
+
 # DATABASE_URI = os.getenv('DATABASE_URI', 'sqlite:///../db/test.db')
 DATABASE_URI = os.getenv(
-    "DATABASE_URI", "postgres://xpwettdf:5e-nu7u_uaDh8zD7NwVnffd8zAQfZC3q@lallah.db.elephantsql.com:5432/xpwettdf"
+    "DATABASE_URI", "postgres://postgres:postgres@localhost:5432/postgres"
 )
 
 # Secret for session management
 # SECRET_KEY = os.getenv("SECRET_KEY", "s3cr3t-key-shhhh")
 
 # override if we are running in Cloud Foundry
-# if 'VCAP_SERVICES' in os.environ:
-#     print("-----DATABASE_URL change to cloud version-----")
-#     vcap = json.loads(os.environ['VCAP_SERVICES'])
-#     DATABASE_URI = vcap['user-provided'][0]['credentials']['url'] 
+if 'VCAP_SERVICES' in os.environ:
+    vcap = json.loads(os.environ['VCAP_SERVICES'])
+    DATABASE_URI = vcap['user-provided'][0]['credentials']['url'] 
 
 # Configure SQLAlchemy
 SQLALCHEMY_DATABASE_URI = DATABASE_URI
-SQLALCHEMY_TRACK_MODIFICATIONS = False 
+SQLALCHEMY_TRACK_MODIFICATIONS = False
