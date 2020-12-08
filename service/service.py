fa498c128439d0c4434ce733864b2849237dc705
nancywen25 <nwen2011@gmail.com>
2020-12-08 00:31:21 -0500

    move first name in front of last name

--- service/service.py
+++ service/service.py
@@ -158,8 +158,8 @@ customer_model = api.inherit(
 
 # query string arguments
 customer_args = reqparse.RequestParser()
-customer_args.add_argument('last_name', type=str, required=False, help='List Customers by last name')
 customer_args.add_argument('first_name', type=str, required=False, help='List Customers by first name')
+customer_args.add_argument('last_name', type=str, required=False, help='List Customers by last name')
 customer_args.add_argument('email', type=str, required=False, help='List Customers by email')
 customer_args.add_argument('address', type=str, required=False, help='List Customers by address')
 customer_args.add_argument('active', type=inputs.boolean, required=False, help='List Customers by availability')
